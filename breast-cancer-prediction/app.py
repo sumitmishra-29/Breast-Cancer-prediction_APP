11db59ccc0a09d2db476729f135ec678da657f0e
sumit mishra <sumitmishra2972002@email.com>
2024-10-24 15:00:09 +0530

    chageb

--- breast-cancer-prediction/app.py
+++ breast-cancer-prediction/app.py
@@ -4,17 +4,11 @@ import pandas as pd
 from flask_cors import CORS
 import numpy  as np
 import pickle
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Hello, Heroku!"
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
-    
 CORS(app)
 # Load the pre-trained model
 with open('model.pkl', 'rb') as model_file:
